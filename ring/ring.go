5f9fc3f7efe7fdcb7cf765d5cb96b938547ace87
Adam Drescher <adam.r.drescher@gmail.com>
2024-04-22 23:59:55 -0400

    ring: add the beginnings of a ring test.
    Also, make some revisions to the ring implementation. Some corrections were necessary
    since I hadn't been compiling up to this point, so the test motivated some obvious fixes.
    
    I'm not pleased with the fact that I need to supply the generic type to each of the functional
    options... it feels like supplying it to `New(...)` on ring creation should be sufficient.
    I need to look into fixing this unpleasant boilerplate one way or another.

--- ring/ring.go
+++ ring/ring.go
@@ -1,27 +1,26 @@
 package ring
 
 import (
+        "errors"
+        "fmt"
         "runtime"
         "sync/atomic"
 )
 
-
-// Ring is a Go port of rte_ring from DPDK. It provides a fixed-size ring buffer
-// that can function as anything between an SPSC and MPMC queue. It defaults to
-// an SPSC queue in the base implementation, and multi-producer/multi-consumer
-// semantics can be added through functional options. So an MPMC queue would be
-// the ring initialized with both the multi-producer and multi-consumer options.
+// Ring is a Go port of rte_ring from DPDK. It provides a fixed-size ring buffer that can function
+// as anything between an SPSC and MPMC queue.
 //
-// It can incur a non-trivial performance hit in order to hold truly generic
-// elements, so if bleeding-edge performance is desired, stick to the types
-// required by the DPDK ring buffer implementation: a pointer, or 4-byte
-// boundary aligned element. Otherwise, an element could be split between
-// adjacent cache lines.
+// The ring defaults to an SPSC queue in the base implementation, and multi-producer/multi-consumer
+// semantics can be added through functional options. For example: to create an MPMC queue,
+// initialize the ring with both the multi-producer and multi-consumer options.
 //
-// The actual number of entries in the ring is *count - 1*, because the *count*
-// entry is used to determine whether the ring is empty or not. If you
-// specifically need *count* elements, see the friendlier-but-slower queue
-// implementation in [TODO reference once I implement this].
+// It can incur a non-trivial performance hit in order to hold truly generic elements, so if
+// bleeding-edge performance is desired, stick to the types required by the DPDK ring buffer
+// implementation: a pointer, or 4-byte boundary aligned element. Otherwise, an element could be
+// split between adjacent cache lines.
+//
+// The actual number of entries in the ring is *count - 1*, because the *count* entry is used to
+// determine whether the ring is empty or not.
 type Ring[T any] struct {
         entries  []T
 
@@ -38,34 +37,36 @@ type Ring[T any] struct {
         multiProdEnqueue bool /* Multi-Producer enqueue instead of single. */
         multiConsDequeue bool /* Multi-Consumer dequeue instead of single. */
 
-        _  [2]byte /* padding for better cache alignment */
+        _ [2]byte /* padding for better cache alignment */
 }
 
-type RingOption func(*Ring)
-
-func WithMultiProdEnqueue() RingOption {
-        return func(ring *Ring) {
+func WithMultiProdEnqueue[T any]() func(*Ring[T]) {
+        return func(ring *Ring[T]) {
                 ring.multiProdEnqueue = true
         }
 }
 
-func WithMultiConsDequeue() RingOption {
-        return func(ring *Ring) {
+func WithMultiConsDequeue[T any]() func(*Ring[T]) {
+        return func(ring *Ring[T]) {
                 ring.multiConsDequeue = true
         }
 }
 
-func New[T any](count uint32, opts ...RingOption) (*Ring[T], error) {
+func New[T any](count uint32, opts ...func(*Ring[T])) (*Ring[T], error) {
         if count % 2 != 0 {
-                return &Ring[T]{}, error.New("count is not a power of 2")
+                return &Ring[T]{}, errors.New("count is not a power of 2")
         }
         mask := count - 1
 
         ring := &Ring[T]{
                 entries:  make([]T, count),
                 size:     count,
-                mask:     mask
+                mask:     mask,
                 capacity: mask,
+                prodHead: new(uint32),
+                prodTail: new(uint32),
+                consHead: new(uint32),
+                consTail: new(uint32),
         }
 
         *ring.prodHead = 0
@@ -77,7 +78,7 @@ func New[T any](count uint32, opts ...RingOption) (*Ring[T], error) {
                 opt(ring)
         }
 
-        return ring
+        return ring, nil
 }
 
 /**
@@ -90,7 +91,8 @@ func New[T any](count uint32, opts ...RingOption) (*Ring[T], error) {
  *   `nil` if the element was successfully enqueued, or an error explaining why if not.
  */
 func (ring *Ring[T]) Enqueue(element T) error {
-        elements := &T{element}
+        elements := make([]T, 1)
+        elements[0] = element
         numEnqueued := ring.doEnqueue(elements, 1, nil, true)
         if numEnqueued != 1 {
                 return errors.New("no space in the ring")
@@ -113,7 +115,7 @@ func (ring *Ring[T]) Enqueue(element T) error {
  *   enqueue the batch.
  */
 func (ring *Ring[T]) EnqueueBulk(
-        elements T[],
+        elements []T,
         numElems uint32,
         freeSpace *uint32,
 ) error {
@@ -142,7 +144,7 @@ func (ring *Ring[T]) EnqueueBulk(
  *   the number of elements successfully enqueued (on the closed interval [0, n]).
  */
 func (ring *Ring[T]) EnqueueBurst(
-        elements T[],
+        elements []T,
         numElems uint32,
         freeSpace *uint32,
 ) uint32 {
@@ -155,14 +157,15 @@ func (ring *Ring[T]) EnqueueBurst(
 }
 
 func (ring *Ring[T]) doEnqueue(
-        elements T[],
+        elements []T,
         numElems uint32,
         freeSpace *uint32,
         fixedEnqueue bool,
 ) uint32 {
         var (
-                prodHead    uint32
-                prodNext    uint32
+                prodHead uint32
+                prodNext uint32
+                i        uint32
         )
 
         // get the number of entries that we have space to accommodate
@@ -178,13 +181,13 @@ func (ring *Ring[T]) doEnqueue(
 
         // enqueue the elements
         if prodNext > prodHead {
-                for i := range numEntries {
-                        ring.entries[prodHead + i] = elements[i]
+                for i = 0; i < numEntries; i++ {
+                        ring.entries[prodHead+i] = elements[i]
                 }
         } else {
                 var idx uint32
                 // use more expensive wrappingAdd since we are wrapping around
-                for i := range numEntries {
+                for i = 0; i < numEntries; i++ {
                         idx = wrappingAdd(prodHead, i, ring.mask)
                         ring.entries[idx] = elements[i]
                 }
@@ -232,16 +235,16 @@ func (ring *Ring[T]) moveProdHead(
         capacity := ring.capacity
         maxItems := numItems
 
-        // Go doesn't have any relaxed memory ordering tools or the ability to
-        // leverage memory fences (from what I can tell), so we will have to 
-        // use sequentially consistent atomic operations for each of these.
+        // Go doesn't have any relaxed memory ordering tools or the ability to leverage memory
+        // fences (from what I can tell), so we will have to use sequentially consistent atomic
+        // operations for each of these.
         *oldHead = atomic.LoadUint32(ring.prodHead)
         for {
                 numItems = maxItems
                 consTail = atomic.LoadUint32(ring.consTail)
 
-                /* uint32 subtraction trickery makes this calculation always
-                 * valid, even if *oldHead > consTail.
+                /* uint32 subtraction trickery makes this calculation always valid, even if
+                 * `*oldHead > consTail`.
                  */
                 freeEntries = (capacity + consTail - *oldHead)
                 if numItems > freeEntries {
@@ -253,13 +256,15 @@ func (ring *Ring[T]) moveProdHead(
 
                 *newHead = wrappingAdd(*oldHead, numItems, ring.mask)
                 if multiProd {
-                        success = atomic.CompareAndSwap(ring.prodHead, *oldHead, *newHead)
+                        success = atomic.CompareAndSwapUint32(ring.prodHead, *oldHead, *newHead)
                 } else {
-                        ring.prodHead = *newHead
+                        *ring.prodHead = *newHead
                         success = true
                 }
 
-                if (success == true) break
+                if success == true {
+                        break
+                }
         }
 
         return numItems, freeEntries
@@ -269,23 +274,20 @@ func (ring *Ring[T]) moveProdHead(
  * moveConsHead moves the ring's consumer head for a dequeue operation.
  *
  * Params:
- *   numItems - The number of items we want to dequeue, i.e., how far the head
- *              should be moved.
- *   oldHead  - Saves the consumer head value from *before* the move (where the
- *              dequeue starts).
- *   newHead  - Saves the producer head value from *after* the move (where the
- *              dequeue ends).
+ *   numItems - The number of items we want to dequeue, i.e., how far the head should be moved.
+ *   oldHead  - Saves the consumer head value from *before* the move (where the dequeue starts).
+ *   newHead  - Saves the producer head value from *after* the move (where the dequeue ends).
  *
  * Returns:
- *   1. The actual number of items enqueued. This can lie anywhere between 0
- *      and numItems (inclusive).
+ *   1. The actual number of items enqueued. This can lie anywhere between 0 and numItems
+        (inclusive).
  *   2. Returns the number of free entries in the ring before the head was moved.
  */
 func (ring *Ring[T]) moveConsHead(
         numItems uint32,
-        // TODO I think we want the updates for these so they should stay pointers,
-        // but once I start using the function, if we only care about feeding the
-        // value in, we could supply straight up uint32s instead
+        // TODO I think we want the updates for these so they should stay pointers, but once I
+        // start using the function, if we only care about feeding the value in, we could supply
+        // straight up uint32s instead
         oldHead *uint32,
         newHead *uint32,
 ) (uint32, uint32) {
@@ -303,8 +305,8 @@ func (ring *Ring[T]) moveConsHead(
                 numItems = maxItems
                 prodTail = atomic.LoadUint32(ring.prodTail)
 
-                /* uint32 subtraction trickery makes this calculation always
-                 * valid, even if *oldHead > prodTail.
+                /* uint32 subtraction trickery makes this calculation always valid, even if
+                 * `*oldHead > prodTail`.
                  */
                 entries = prodTail - *oldHead
                 if numItems > entries {
@@ -313,55 +315,56 @@ func (ring *Ring[T]) moveConsHead(
 
                 *newHead = *oldHead + numItems
                 if multiCons {
-                        success = atomic.CompareAndSwap(ring.consHead, *oldHead, *newHead)
+                        success = atomic.CompareAndSwapUint32(ring.consHead, *oldHead, *newHead)
                 } else {
-                        ring.consHead = *newHead
+                        *ring.consHead = *newHead
                         success = true
                 }
 
-                if (success == true) break
+                if success == true {
+                        break
+                }
         }
 
         return numItems, entries
 }
 
 /**
- * updateTail updates the tail for the ring. Since it takes an arbitrary ring,
- * it can handle both enqueue (passing the producer tail) and dequeue (passing
- * the consumer tail).
+ * updateTail updates the tail for the ring. Since it takes an arbitrary ring, it can handle both
+ * enqueue (passing the producer tail) and dequeue (passing the consumer tail).
  *
  * Params:
  *   tailPtr - a pointer to ring's tail index that should be moved.
- *   multi   - whether this ring operation supports multiple producers/consumers.
- *   oldTail - the tail value that we need to see in order to start our tail
- *             update. In the multiple producer/consumer case, we must wait for
- *             operations in front of us to update the tail before we can go.
+ *   multi - whether this ring operation supports multiple producers/consumers.
+ *   oldTail - the tail value that we need to see in order to start our tail update. In the multiple
+ *       producer/consumer case, we must wait for operations in front of us to update the tail before
+ *       we can go.
  *   newTail - the tail value that we are updating the ring tail to reflect.
  */
 func updateTail(
         tailPtr *uint32,
-        multi    bool,
-        oldTail  uint32,
-        newTail  uint32,
-){
+        multi bool,
+        oldTail uint32,
+        newTail uint32,
+) {
         if multi {
                 // need to wait for the other enqueues/dequeues that precede us
                 // to complete, before we can go on.
-                for !atomic.CompareAndSwap(tailPtr, oldTail, newTail) {
+                for !atomic.CompareAndSwapUint32(tailPtr, oldTail, newTail) {
                         runtime.Gosched()
                 }
                 return
         }
 
-        // TODO why does the single-producer/single-consumer (aka serialized)
-        // case have an atomic op here? Can't we just assign it straight up?
+        // TODO why does the single-producer/single-consumer (aka serialized) case have an atomic op
+        // here? Can't we just assign it straight up?
         // atomic.StoreUint32(ring.prodTail, newTail)
         *tailPtr = newTail
 }
 
-// Note: this is only valid if we can perform `base + addend` without overflowing
-// the uint32. However, this is only a danger if someone is trying to create a
-// ring of size 2^32 (which would be >4 billion entries) so we should be safe.
-func wrappingAdd(uint32 base, uint32 addend, uint32 mask) uint32 {
+// Note: this is only valid if we can perform `base + addend` without overflowing the uint32.
+// However, this is only a danger if someone is trying to create a ring of size 2^32 (which would
+// be >4 billion entries) so we should be safe.
+func wrappingAdd(base, addend, mask uint32) uint32 {
         return (base + addend) & mask
 }
